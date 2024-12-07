07e5ed6c99f8a5f2e242c1118856a77a55755b9b
Daniel_C <daniel.curteza@gmail.com>
2024-12-07 08:42:35 +0200

    Modified framework for further tests and added further tests

--- src/test/java/tests/CartProductsPersistenceTest.java
+++ src/test/java/tests/CartProductsPersistenceTest.java
@@ -4,11 +4,5 @@ import org.testng.annotations.Test;
 import pages.SearchProductPage;
 
 public class CartProductsPersistenceTest extends BaseTest{
-    @Test(dependsOnMethods = {"AddToCartTest.searchAddToCartTest"})
-    public void testCartProductsPersistence() {
-        setUp();
-        navigateToURL("");
-    }
-    SearchProductPage searchProductPage = new SearchProductPage(driver);
 
 }
